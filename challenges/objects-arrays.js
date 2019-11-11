f040a50a16827526b0feabd88dc5f74728ca771a
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2019-11-10 21:39:54 -0500

    most object/array questions done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,6 +6,18 @@
   object name, diet, weight, length, period
 */
 
+const Dinosaur = function(name, diet, weight, length, period) {
+  this.name = name; 
+  this.diet = diet;
+  this.weight = weight;
+  this.length = length;
+  this.period = period;
+}
+
+const tyrannosaurus = new Dinosaur('Tyrannosaurus', 'carnivorous', '7000kg', '12m','Late Cretaceous');
+const stegosaurus = new Dinosaur('stegosaurus', 'herbivorous', '2000kg', '9m', 'Late Jurassic');
+const velociraptor = new Dinosaur('velociraptor', 'carnivorous', '15kg', '1.8m', 'Late Cretaceous');
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +27,25 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+tyrannosaurus.roar = function() {
+  console.log("RAWERSRARARWERSARARARRRR!");
+}
+
+tyrannosaurus.roar();
 
 
 // ==== Arrays ====
@@ -50,8 +67,17 @@ const graduates = [
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
+
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+
+for (i = 0; i < graduates.length; i++) {
+  graduates[i].university;    
+  universities.push(graduates[i].university);
+  universities.sort();
+}
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -61,10 +87,18 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+for(i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} " " ${graduates[i].email} `);
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for(i = 0; i < graduates.length; i++) {
+  graduates[i].university;
+  if (university === Uni)
+    unisWithUni.push(graduates[i].university);
+}
 console.log(unisWithUni);
 
 
@@ -90,8 +124,7 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
-console.log(displayNames);
+// NOT DONE
 
 /* Request 2: .map()
 
@@ -99,7 +132,11 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((zooAnimal, index) => {
+  return zooAnimal.animal_name.toLowerCase();
+
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -107,8 +144,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-console.log(lowPopulationAnimals);
+
 
 /* Request 4: .reduce() 
 
